1b8fcb6e515659ce8f5b92cedb449252bcdddad5
BronzeToad <github@ajpf.me>
2023-09-05 23:12:24 -0500

    Update file_type_validator.py

--- toad_tools/file_type_validator.py
+++ toad_tools/file_type_validator.py
@@ -18,315 +18,199 @@ from toad_tools.file_utils import check_filepath
 
 # =========================================================================== #
 
-def file_type_validator(
-    file_path: Union[str, Path],
-    file_type: FileType
-) -> bool:
+class FileTypeValidator:
     """
-    Validates a file against its expected file type.
+    This class provides methods for validating files against their expected types.
 
-    This function checks if a given file matches its expected file type by
-    invoking the corresponding validation function from a pre-defined map of
-    type validators.
+    Attributes:
+        file_path (str): The path to the file that needs to be validated.
+        file_type (FileType): The expected type of the file. This should be an
+            enum or similar type.
 
-    Args:
-        file_path (Union[str, Path]): Path to file that needs to be validated.
-        file_type (FileType): An enum representing the expected file type.
-
-    Returns:
-        bool: True if the file is a valid instance of the expected file type,
-            False otherwise.
-
-    Raises:
-        Warning: If the file type is not supported for validation, or if
-            the validation fails, or if an error occurs during validation.
-    """
-    type_validator_map = get_type_validator_map()
-
-    if file_type not in type_validator_map:
-        return "File type not supported for validation."
-
-    check_filepath(file_path, FileCheckType.NOT_FOUND)
-
-    try:
-        with open(file_path, 'r') as file:
-            if not type_validator_map[file_type](file):
-                warn(f"Invalid {file_type.name} file.")
-                return False
-    except Exception as e:
-        warn(f"Unable to validate {file_path}: {str(e)}.")
-        return False
-
-    return True
-
-
-def get_type_validator_map():
-    """
-    Returns a dictionary mapping file types to validation functions.
-
-    This function provides a map of FileType enums to validation functions,
-    which can be used to check if a given file adheres to the expected format
-    or structure for its type. The validation functions should accept a file
-    object and return a boolean indicating the validity of the file.
-
-    Returns:
-        dict: A dictionary where the keys are FileType enums and the values are
-            functions that take a file object and return a boolean.
+    Methods:
+        validate(): Validates the file against its expected type and returns a
+            boolean indicating the validity.
     """
 
-    return {
-        FileType.JSON: _validate_json,
-        FileType.HTML: _validate_html,
-        FileType.SQL : _validate_sql,
-        FileType.XML : _validate_xml,
-        FileType.CSV : _validate_csv,
-        FileType.YAML: _validate_yaml,
-        FileType.TXT : _validate_text,
-        FileType.MD  : _validate_text,
-        FileType.INI : _validate_ini,
-        FileType.LOG : _validate_text,
-        FileType.CONF: _validate_text,
-        FileType.PY  : _validate_text,
-        FileType.JS  : _validate_text,
-        FileType.CSS : _validate_text,
-        FileType.JPG : _validate_image,
-        FileType.PNG : _validate_image,
-        FileType.GIF : _validate_image,
-        FileType.BMP : _validate_image,
-        FileType.TIFF: _validate_image,
-        FileType.PDF : _validate_pdf,
-        FileType.MP3 : _validate_audio,
-        FileType.WAV : _validate_audio,
-        FileType.FLAC: _validate_audio,
-        FileType.MP4 : _validate_video,
-        FileType.AVI : _validate_video,
-        FileType.MKV : _validate_video,
-        FileType.MOV : _validate_video,
-        FileType.WMV : _validate_video
-    }
+    def __init__(
+        self,
+        file_path: Union[str, Path],
+        file_type: FileType
+    ):
+        self.file_path = str(file_path)
+        self.file_type = file_type
 
-def _validate_image(f):
-    """
-    Validates if a given file is a readable image.
-
-    This function uses the PIL library to attempt to open and verify the image.
-
-    Args:
-        f (file): The file object to be validated.
-
-    Returns:
-        bool: True if the file is a valid image, False otherwise.
-    """
-    try:
-        with Image.open(f) as img:
-            img.verify()
-        return True
-    except:
-        return False
-
-def _validate_audio(f):
-    """
-    Validates if a given file is a readable audio file.
-
-    This function uses the SoundFile library to attempt to open the audio file.
-
-    Args:
-        f (file): The file object to be validated.
-
-    Returns:
-        bool: True if the file is a valid audio file, False otherwise.
-    """
-    try:
-        SoundFile(f)
-        return True
-    except:
-        return False
-
-def _validate_video(f):
-    """
-    Validates if a given file is a readable video file.
-
-    This function uses the OpenCV library to attempt to open and
-    verify the video file.
-
-    Args:
-        f (file): The file object to be validated.
-
-    Returns:
-        bool: True if the file is a valid video file, False otherwise.
-    """
-    try:
-        video = VideoCapture(f)
-        return video.isOpened()
-    except:
-        return False
-
-def _validate_pdf(f):
-    """
-    Validates a PDF file.
-
-    This function checks if a given PDF file is readable and not encrypted.
-
-    Args:
-        f (file): A file object opened in read mode.
-
-    Returns:
-        bool: True if the file is a valid PDF, False otherwise.
-
-    Raises:
-        Warning: If the PDF is encrypted.
-    """
-    try:
-        reader = PdfFileReader(f)
-        if reader.isEncrypted:
-            warn("PDF file is encrypted, unable to validate...")
+    def validate(self) -> bool:
+        """Validates a file against its expected file type."""
+        type_validator_map = self._get_type_validator_map()
+        check_filepath(self.file_path, FileCheckType.NOT_FOUND)
+        if self.file_type not in type_validator_map:
+            warn("File type not supported for validation.")
             return False
-        return True
-    except:
-        return False
+        try:
+            return type_validator_map[self.file_type]()
+        except Exception as e:
+            warn(f"Unable to validate {self.file_path}: {str(e)}.")
+            return False
 
-def _validate_json(f):
-    """
-    Validates a JSON file.
+    def _get_type_validator_map(self) -> dict:
+        """Returns a dictionary mapping file types to validation functions."""
+        return {
+            FileType.JSON: self._validate_json,
+            FileType.HTML: self._validate_html,
+            FileType.SQL : self._validate_sql,
+            FileType.XML : self._validate_xml,
+            FileType.CSV : self._validate_csv,
+            FileType.YAML: self._validate_yaml,
+            FileType.TXT : self._validate_text,
+            FileType.MD  : self._validate_text,
+            FileType.INI : self._validate_ini,
+            FileType.LOG : self._validate_text,
+            FileType.CONF: self._validate_text,
+            FileType.PY  : self._validate_text,
+            FileType.JS  : self._validate_text,
+            FileType.CSS : self._validate_text,
+            FileType.JPG : self._validate_image,
+            FileType.PNG : self._validate_image,
+            FileType.GIF : self._validate_image,
+            FileType.BMP : self._validate_image,
+            FileType.TIFF: self._validate_image,
+            FileType.PDF : self._validate_pdf,
+            FileType.MP3 : self._validate_audio,
+            FileType.WAV : self._validate_audio,
+            FileType.FLAC: self._validate_audio,
+            FileType.MP4 : self._validate_video,
+            FileType.AVI : self._validate_video,
+            FileType.MKV : self._validate_video,
+            FileType.MOV : self._validate_video,
+            FileType.WMV : self._validate_video
+        }
 
-    This function checks if a given JSON file is properly formatted.
 
-    Args:
-        f (file): A file object opened in read mode.
+    def _validate_image(self) -> bool:
+        """Validates if a given file is a readable image."""
+        try:
+            with Image.open(self.file_path) as img:
+                img.verify()
+            return True
+        except Exception as e:
+            warn(f"Invalid Image file: {self.file_path}, Error: {e}")
+            return False
 
-    Returns:
-        bool: True if the file is a valid JSON, False otherwise.
-    """
-    try:
-        json.load(f)
-        return True
-    except:
-        return False
 
-def _validate_html(f):
-    """
-    Validates an HTML file.
+    def _validate_audio(self) -> bool:
+        """Validates if a given file is a readable audio file."""
+        try:
+            with SoundFile(self.file_path):
+                pass
+            return True
+        except Exception as e:
+            warn(f"Invalid Audio file: {self.file_path}, Error: {e}")
+            return False
 
-    This function checks if a given HTML file contains the '<html>' tag,
-    a basic indicator of an HTML file.
+    def _validate_video(self) -> bool:
+        """Validates if a given file is a readable video file."""
+        try:
+            video = VideoCapture(self.file_path)
+            isOpened = video.isOpened()
+            video.release()
+            return isOpened
+        except Exception as e:
+            warn(f"Invalid Video file: {self.file_path}, Error: {e}")
+            return False
 
-    Args:
-        f (file): A file object opened in read mode.
+    def _validate_pdf(self) -> bool:
+        """Validates a PDF file."""
+        try:
+            with open(self.file_path, 'rb') as f:
+                reader = PdfFileReader(f)
+                if reader.isEncrypted:
+                    warn("PDF file is encrypted, unable to validate.")
+                    return False
+            return True
+        except Exception as e:
+            warn(f"Invalid PDF file: {self.file_path}, Error: {e}")
+            return False
 
-    Returns:
-        bool: True if the file is a valid HTML, False otherwise.
-    """
-    try:
-        return '<html>' in f.read().lower()
-    except:
-        return False
+    def _validate_json(self) -> bool:
+        """Validates a JSON file."""
+        try:
+            with open(self.file_path, 'r') as f:
+                json.load(f)
+            return True
+        except json.JSONDecodeError:
+            warn(f"Invalid JSON file: {self.file_path}")
+            return False
 
-def _validate_xml(f):
-    """
-    Validates if the given file is a well-formed XML file.
+    def _validate_csv(self) -> bool:
+        """Validates a CSV file."""
+        try:
+            with open(self.file_path, 'r') as f:
+                reader = csv.reader(f)
+                next(reader)
+            return True
+        except csv.Error:
+            warn(f"Invalid CSV file: {self.file_path}")
+            return False
 
-    Args:
-        f (file object): A file object representing the XML file.
+    def _validate_html(self) -> bool:
+        """Validates an HTML file."""
+        try:
+            with open(self.file_path, 'r') as f:
+                return '<html>' in f.read().lower()
+        except Exception as e:
+            warn(f"Invalid HTML file: {self.file_path}, Error: {e}")
+            return False
 
-    Returns:
-        bool: True if the file is a well-formed XML, False otherwise.
-    """
-    try:
-        ET.parse(f)
-        return True
-    except:
-        return False
+    def _validate_xml(self) -> bool:
+        """Validates if the given file is a well-formed XML file."""
+        try:
+            ET.parse(self.file_path)
+            return True
+        except ET.ParseError:
+            warn(f"Invalid XML file: {self.file_path}")
+            return False
 
-def _validate_yaml(f):
-    """
-    Validates if the given file is a well-formed YAML file.
+    def _validate_yaml(self) -> bool:
+        """Validates if the given file is a well-formed YAML file."""
+        try:
+            with open(self.file_path, 'r') as f:
+                yaml.safe_load(f)
+            return True
+        except yaml.YAMLError:
+            warn(f"Invalid YAML file: {self.file_path}")
+            return False
 
-    Args:
-        f (file object): A file object representing the YAML file.
+    def _validate_sql(self) -> bool:
+        """Validates if the given file contains SQL keywords."""
+        sql_keywords = [
+            'select', 'from', 'where', 'insert', 'update',
+            'delete', 'create', 'alter', 'drop'
+        ]
+        try:
+            with open(self.file_path, 'r') as f:
+                content = f.read().lower()
+            return any(keyword in content for keyword in sql_keywords)
+        except Exception as e:
+            warn(f"Invalid SQL file: {self.file_path}, Error: {e}")
+            return False
 
-    Returns:
-        bool: True if the file is a well-formed YAML, False otherwise.
-    """
-    try:
-        yaml.safe_load(f)
-        return True
-    except:
-        return False
+    def _validate_ini(self) -> bool:
+        """Validates if the file object contains valid INI data."""
+        try:
+            config = ConfigParser()
+            with open(self.file_path, 'r') as f:
+                config.read_file(f)
+            return True
+        except Exception as e:
+            warn(f"Invalid INI file: {self.file_path}, Error: {e}")
+            return False
 
-def _validate_sql(f):
-    """
-    Validates if the given file is a SQL file containing SQL keywords.
-
-    Args:
-        f (file object): A file object representing the SQL file.
-
-    Returns:
-        bool: True if the file contains SQL keywords, False otherwise.
-    """
-    sql_keywords = [
-        'select', 'from', 'where', 'insert', 'update',
-        'delete', 'create', 'alter', 'drop'
-    ]
-    try:
-        content = f.read().lower()
-        return any(keyword in content for keyword in sql_keywords)
-    except:
-        return False
-
-def _validate_csv(f):
-    """
-    Validates if the file object contains valid CSV data.
-
-    This function reads the file object to see if it conforms to the CSV
-    (Comma Separated Values) format. It uses Python's built-in `csv.reader`.
-
-    Args:
-        f (file object): A file object opened in read mode.
-
-    Returns:
-        bool: True if the file is a valid CSV file, False otherwise.
-    """
-    try:
-        reader = csv.reader(f)
-        next(reader)
-        return True
-    except:
-        return False
-
-def _validate_ini(f):
-    """
-    Validates if the file object contains valid INI data.
-
-    This function reads the file object to see if it conforms to the INI
-    format. It uses Python's built-in `ConfigParser`.
-
-    Args:
-        f (file object): A file object opened in read mode.
-
-    Returns:
-        bool: True if the file is a valid INI file, False otherwise.
-    """
-    try:
-        config = ConfigParser()
-        config.read_file(f)
-        return True
-    except:
-        return False
-
-def _validate_text(f):
-    """
-    Validates if the file object contains readable text.
-
-    This function reads the file object to check if it contains any text.
-    It returns True if the file is not empty.
-
-    Args:
-        f (file object): A file object opened in read mode.
-
-    Returns:
-        bool: True if the file is not empty, False otherwise.
-    """
-    try:
-        return bool(f.read())
-    except:
-        return False
+    def _validate_text(self) -> bool:
+        """Validates if the file object contains readable text."""
+        try:
+            with open(self.file_path, 'r') as f:
+                return bool(f.read())
+        except Exception as e:
+            warn(f"Invalid Text file: {self.file_path}, Error: {e}")
+            return False
