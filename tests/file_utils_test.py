4ccca43a9f98a410943f10252ab5f004b9dd5089
BronzeToad <github@ajpf.me>
2023-09-06 00:31:28 -0500

    Update file_utils_test.py

---
+++ tests/file_utils_test.py
@@ -0,0 +1,57 @@
+import pytest
+import src.string_utils as fileutils
+
+
+# =========================================================================== #
+
+# test get_file_type_map
+
+# test check_filepath
+
+# test force_extension
+
+# test get_file
+
+# test duplicate_file
+
+# test delete_file
+
+# test rename_file
+
+# test ensure_directory_exists
+
+# test directory_cleanup
+
+# test get_file_size
+
+# test get_last_modified
+
+# test validate_file_type
+
+# test concatenate_files
+
+# test count_lines
+
+# test convert_file_encoding
+
+# test search_text_in_file
+
+# test calculate_checksum
+
+# test bulk_rename
+
+# test bulk_move_copy
+
+# test lock_file
+
+# test save_versioned_file
+
+# test _serialize
+
+# test _deserialize
+
+# test serialize_deserialize
+
+# test read_large_file_in_chunks
+
+# test write_large_string_in_chunks
