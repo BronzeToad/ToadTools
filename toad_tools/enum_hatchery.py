d2b54a327edcd5c629df694f2f4954408fd1b263
BronzeToad <github@ajpf.me>
2023-09-05 21:44:47 -0500

    Update enum_hatchery.py

--- toad_tools/enum_hatchery.py
+++ toad_tools/enum_hatchery.py
@@ -20,6 +20,23 @@ class FileCheckType(Enum):
     EXISTS = auto()
     NOT_FOUND = auto()
 
+class CurrencyType(Enum):
+    USD = {'symbol': '$', 'placement': 'before', 'decimal_places': 2}
+    EUR = {'symbol': '€', 'placement': 'before', 'decimal_places': 2}
+    GBP = {'symbol': '£', 'placement': 'before', 'decimal_places': 2}
+    JPY = {'symbol': '¥', 'placement': 'before', 'decimal_places': 0}
+    INR = {'symbol': '₹', 'placement': 'before', 'decimal_places': 2}
+    AUD = {'symbol': 'A$', 'placement': 'before', 'decimal_places': 2}
+    CAD = {'symbol': 'C$', 'placement': 'before', 'decimal_places': 2}
+    CHF = {'symbol': 'CHF', 'placement': 'after', 'decimal_places': 2}
+    CNY = {'symbol': '¥', 'placement': 'before', 'decimal_places': 2}
+    BRL = {'symbol': 'R$', 'placement': 'before', 'decimal_places': 2}
+    MXN = {'symbol': 'MX$', 'placement': 'before', 'decimal_places': 2}
+    NZD = {'symbol': 'NZ$', 'placement': 'before', 'decimal_places': 2}
+    NOK = {'symbol': 'kr', 'placement': 'before', 'decimal_places': 2}
+    RUB = {'symbol': '₽', 'placement': 'before', 'decimal_places': 2}
+    ZAR = {'symbol': 'R', 'placement': 'before', 'decimal_places': 2}
+
 class FileType(Enum):
     JSON = "json"
     HTML = "html"
