f2980153a09a9e72fde7b77cf7fc9aebeabc5f7e
BronzeToad <github@ajpf.me>
2023-10-03 07:21:08 -0500

    Update github.py

--- check_urls/github.py
+++ check_urls/github.py
@@ -1,9 +1,25 @@
+import requests
 
-
+import utilities as Utils
+from utilities import ConfigType
 
 # =========================================================================== #
 
+def get_base_api_url() -> str:
+    """Retrieve the GitHub API URL."""
+    return Utils.get_config_val(
+        section='GitHub',
+        key='BASE_API_URL'
+    )
 
+def get_batch_size() -> int:
+    """Retrieve the batch size from the config file."""
+    return Utils.get_config_val('Github', 'BATCH_SIZE')
+
+
+def get_max_retries() -> int:
+    """Retrieve the max retries from the config file."""
+    return Utils.get_config_val('Github', 'MAX_RETRIES')
 
 # =========================================================================== #
 
