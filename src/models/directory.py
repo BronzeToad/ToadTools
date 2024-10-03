72ef059a7de8c07d9ab4b0f914f2a0c6c14ce7b8
BronzeToad <github@ajpf.me>
2024-10-03 11:45:49 -0500

    refactored as non-dataclass

--- src/models/directory.py
+++ src/models/directory.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Union, Optional, List
 
@@ -8,242 +7,258 @@ frog = ToadLogger("models.directory", level=LogLevel.DEBUG)
 
 DISALLOWED_CHARS: List[str] = ["<", ">", ":", '"', "/", "\\", "|", "?", "*", "\0"]
 
-RESERVED_NAMES: List[str] = [
-    "CON",
-    "PRN",
-    "AUX",
-    "NUL",
-    "COM1",
-    "COM2",
-    "COM3",
-    "COM4",
-    "COM5",
-    "COM6",
-    "COM7",
-    "COM8",
-    "COM9",
-    "LPT1",
-    "LPT2",
-    "LPT3",
-    "LPT4",
-    "LPT5",
-    "LPT6",
-    "LPT7",
-    "LPT8",
-    "LPT9",
-]
 
-# TODO: add method for deleting directory
-# TODO: add method for renaming directory
-#  will need to update dirname and path after renaming
-#  dirname and path will need to be properties with setters
-#  add a check for reserved names and disallowed chars before renaming
-#  add a check for existing directory with new name before renaming
-
-
-@dataclass
 class Directory:
-    """Represents a directory with validation and creation capabilities.
+    """Represents a directory with validation, creation, deletion, and renaming capabilities.
 
     Attributes:
         dirname (str): The name of the directory.
         parent (Union[str, Path]): The path to the parent directory.
-        create_if_not_exists (bool): Whether to create the directory if it does not exist.
-            Defaults to False.
         replacement_char (Optional[str]): Character to replace disallowed characters in dirname.
             Defaults to None.
-        os_reserved_dirnames (List[str]): List of OS-reserved directory names.
-            Defaults to RESERVED_NAMES.
         disallowed_chars (List[str]): List of characters not allowed in the dirname.
             Defaults to DISALLOWED_CHARS.
     """
 
-    dirname: str
-    parent: Union[str, Path]
-    create_if_not_exists: bool = False
-    replacement_char: Optional[str] = None
-    os_reserved_dirnames: List[str] = field(
-        repr=False, default_factory=lambda: RESERVED_NAMES
-    )
-    disallowed_chars: List[str] = field(
-        repr=False, default_factory=lambda: DISALLOWED_CHARS
-    )
-    path: Optional[Path] = field(init=False, default=None)
+    def __init__(
+        self,
+        dirname: str,
+        parent: Union[str, Path],
+        replacement_char: Optional[str] = None,
+    ):
+        self._dirname = dirname
+        self._parent = parent
+        self._replacement_char = replacement_char
+        self.disallowed_chars = DISALLOWED_CHARS
+        self.__post_init__()
 
-    def __post_init__(self) -> None:
-        """Post-initialization processing to validate and build the directory path.
-
-        Raises:
-            ValueError: If the directory name is reserved by the operating system.
-            ValueError: If disallowed characters are present and no replacement char is provided.
-        """
+    def __post_init__(self):
+        """Post-initialization processing to validate and set attribute values."""
         frog.debug("Directory object created...")
-        self.dirname = self.dirname.strip()
-        self._validate_replacement_char()
+        self.dirname = self._dirname
+        self.parent = self._parent
+        self.replacement_char = self._replacement_char
 
-        if self._check_dirname_for_reserved_names():
-            raise ValueError("Directory name is reserved by the operating system.")
+    def __repr__(self):
+        """Overloaded method to return a string representation of the Directory object."""
+        return (
+            f"Directory("
+            f"dirname: {self.dirname}, "
+            f"parent: {self.parent}, "
+            f"replacement_char: {self.replacement_char}, "
+            f"path: {self.path}, "
+            f"exists: {self.exists}"
+            f")"
+        )
 
-        if self._check_dirname_for_disallowed_chars():
-            raise ValueError("Directory name contains disallowed character.")
+    @property
+    def dirname(self) -> str:
+        """Gets the directory name.
 
-        self.parent = Path(self.parent).resolve()
-        self.path = self._build_full_path()
-
-    def _validate_replacement_char(self) -> None:
-        """Validates the replacement character for disallowed characters.
-
-        Raises:
-            ValueError: If the replacement character is invalid.
+        Returns:
+            str: The directory name.
         """
-        if self.replacement_char in self.disallowed_chars:
-            frog.error(f"Replacement character '{self.replacement_char}' is not valid.")
-            raise ValueError("Invalid replacement character.")
-        if self.replacement_char and len(self.replacement_char) > 1:
+        return self._dirname
+
+    @dirname.setter
+    def dirname(self, value: str) -> None:
+        """Sets the directory name.
+
+        Args:
+            value (str): The directory name.
+        """
+        v = value.strip()
+
+        if Path(v).is_reserved():
+            frog.error(f"Directory name '{v}' is reserved by the operating system.")
+            return
+
+        for char in self.disallowed_chars:
+            while v.startswith(char) or v.endswith(char):
+                v = self._strip_char(v, char)
+
+        for char in self.disallowed_chars:
+            if char in v and self.replacement_char:
+                frog.debug(
+                    f"Replacing disallowed character '{char}' in directory name '{v}' "
+                    f"with '{self.replacement_char}'."
+                )
+                v = v.replace(char, self.replacement_char)
+            elif char in v and not self.replacement_char:
+                frog.error(
+                    f"Directory name '{v}' contains disallowed character '{char}'. Unable to set new value"
+                )
+                return
+
+        frog.debug(f"Setting dirname to '{v}'")
+        self._dirname = v
+
+    @property
+    def parent(self) -> Union[str, Path]:
+        """Gets the parent directory path.
+
+        Returns:
+            Union[str, Path]: The parent directory path.
+        """
+        return self._parent
+
+    @parent.setter
+    def parent(self, value: Union[str, Path]) -> None:
+        """Sets the parent directory path.
+
+        Args:
+            value (Union[str, Path]): The parent directory path.
+        """
+        if isinstance(value, str):
+            v = Path(value.strip()).resolve()
+        else:
+            v = value.resolve()
+
+        if not v.is_dir():
+            frog.error(f"Parent directory '{v}' does not exist.")
+            return
+
+        frog.debug(f"Setting parent to '{v}'")
+        self._parent = v
+
+    @property
+    def replacement_char(self) -> Optional[str]:
+        """Gets the replacement character for disallowed characters in the directory name.
+
+        Returns:
+            Optional[str]: The replacement character.
+        """
+        return self._replacement_char
+
+    @replacement_char.setter
+    def replacement_char(self, value: Optional[str]) -> None:
+        """Sets the replacement character for disallowed characters in the directory name.
+
+        Args:
+            value (Optional[str]): The replacement character.
+        """
+        v = value.strip() if value else value
+        if v in self.disallowed_chars:
+            frog.error(f"Replacement character '{v}' is not valid.")
+            v = None
+        elif v and len(v) > 1:
             frog.error(
-                f"Replacement character '{self.replacement_char}' is too long. "
-                f"Must be a single character."
+                f"Replacement character '{v}' is too long. Must be a single character"
             )
-            raise ValueError("Replacement character too long.")
+            v = None
 
-    def _check_dirname_for_reserved_names(self, dirname: Optional[str] = None) -> bool:
-        """Checks if the directory name is reserved by the operating system.
+        frog.debug(f"Setting replacement_char to '{v}'")
+        self._replacement_char = v
 
-        Args:
-            dirname (str): The directory name to check. Defaults to self.dirname.
-
-        Returns:
-            bool: True if the directory name is reserved, False otherwise.
-        """
-        dirname = dirname or self.dirname
-        if dirname.upper() in self.os_reserved_dirnames:
-            frog.error(f"Directory name '{dirname}' is reserved by OS.")
-            return True
-        return False
-
-    def _check_dirname_for_disallowed_chars(
-        self, dirname: Optional[str] = None
-    ) -> bool:
-        """Checks and handles disallowed characters in the directory name.
-
-        Args:
-            dirname (str): The directory name to check. Defaults to self.dirname.
+    @property
+    def path(self) -> Optional[Path]:
+        """Gets the full path to the directory.
 
         Returns:
-            bool: True if disallowed characters are present and no replacement char is provided,
-                False otherwise.
-        """
-
-        dirname = dirname or self.dirname
-
-        def _strip_char(_dirname: str, _char: str) -> str:
-            """Strips a disallowed character from the start or end of the directory name.
-
-            Args:
-                _dirname (str): The directory name.
-                _char (str): The disallowed character to strip.
-
-            Returns:
-                str: The sanitized directory name.
-            """
-            if _dirname.startswith(_char):
-                _dirname = _dirname[1:]
-                frog.debug(
-                    f"Removed disallowed character '{_char}' from start of directory name."
-                )
-            if _dirname.endswith(_char):
-                _dirname = _dirname[:-1]
-                frog.debug(
-                    f"Removed disallowed character '{_char}' from end of directory name."
-                )
-            return _dirname
-
-        for char in self.disallowed_chars:
-            while dirname.startswith(char) or dirname.endswith(char):
-                dirname = _strip_char(dirname, char)
-
-        for char in self.disallowed_chars:
-            if char in dirname:
-                if self.replacement_char:
-                    frog.debug(
-                        f"Replacing disallowed character '{char}' in directory '{dirname}' "
-                        f"with '{self.replacement_char}'."
-                    )
-                    dirname = dirname.replace(char, self.replacement_char)
-                else:
-                    frog.error(
-                        f"Directory name '{dirname}' contains disallowed character '{char}'."
-                    )
-                    return True
-
-        self.dirname = dirname
-        return False
-
-    def _build_full_path(self) -> Path:
-        """Builds the full path for the directory.
-
-        Returns:
-            Path: The full directory path.
-
-        Raises:
-            ValueError: If the directory does not exist and creation is not enabled.
+            Optional[Path]: The full path to the directory, or None if the directory does not exist.
         """
         if self.dirname == self.parent.name:
             frog.info(
                 f"Directory name '{self.dirname}' already included in parent path."
             )
-            path = self.parent
+            return self.parent
         else:
-            path = self.parent / self.dirname
+            return self.parent / self.dirname
 
-        if self.create_if_not_exists:
-            self.create_directory(path)
+    @property
+    def exists(self) -> bool:
+        """Checks if the directory exists.
 
-        if not path.exists():
-            frog.error(f"Directory '{path}' does not exist.")
-            raise ValueError("Directory does not exist.")
-
-        return path
+        Returns:
+            bool: True if the directory exists, False otherwise.
+        """
+        return self.path.is_dir()
 
     @staticmethod
-    def create_directory(path: Path) -> bool:
-        """Creates the directory at the specified path if it does not exist.
+    def _strip_char(dirname: str, char: str) -> str:
+        """Strips a disallowed character from the start or end of the directory name.
 
         Args:
-            path (Path): The path where the directory should be created.
+            dirname (str): The directory name.
+            char (str): The disallowed character to strip.
+
+        Returns:
+            str: The sanitized directory name.
+        """
+        if dirname.startswith(char):
+            dirname = dirname[1:]
+            frog.debug(
+                f"Removed disallowed character '{char}' from start of directory name."
+            )
+        if dirname.endswith(char):
+            dirname = dirname[:-1]
+            frog.debug(
+                f"Removed disallowed character '{char}' from end of directory name."
+            )
+        return dirname
+
+    def create(self) -> bool:
+        """Creates Directory.path if it does not exist.
 
         Returns:
             bool: True if the directory exists after the operation, False otherwise.
         """
-        if not path.exists():
-            frog.info(f"Creating directory '{path}'...")
-            path.mkdir(parents=True, exist_ok=True)
+        if self.path.exists():
+            frog.info(f"Directory '{self.path}' already exists.")
         else:
-            frog.info(f"Directory '{path}' already exists.")
-        return path.exists()
+            frog.info(f"Creating directory '{self.path}'...")
+            self.path.mkdir(parents=True, exist_ok=True)
+
+        return self.path.exists()
+
+    def delete(self) -> bool:
+        """Deletes Directory.path if it exists.
+
+        Returns:
+            bool: True if the directory does not exist after the operation, False otherwise.
+        """
+        if self.path.exists():
+            frog.info(f"Deleting directory '{self.path}'...")
+            self.path.rmdir()
+        else:
+            frog.info(f"Directory '{self.path}' does not exist.")
+        return not self.path.exists()
+
+    def rename(self, new_name: str) -> bool:
+        """Renames the directory to the specified name.
+
+        Args:
+            new_name (str): The new directory name.
+
+        Returns:
+            bool: True if the directory was successfully renamed, False otherwise.
+        """
+        if self.dirname == new_name:
+            frog.info(f"Directory name '{new_name}' is the same as the current name.")
+            return True
+
+        if not self.exists:
+            frog.error(f"Directory '{self.path}' does not exist. Unable to rename.")
+            return False
+
+        frog.info(f"Renaming directory '{self.dirname}' to '{new_name}'...")
+        new_path = self.path.with_name(new_name)
+        self.path.rename(new_path)
+        self.dirname = new_name
+        return True
 
 
 if __name__ == "__main__":
     # Example usage
-    test_dirnames: List[str] = [
-        "<test>",
-        "test",
-        "test/",
-        "test\\",
-        "te/st//",
-        "te\\st\\\\",
-        "*t*e*st*",
-    ]
-
-    for dirname in test_dirnames:
-        frog.error(f"\n\nTesting directory name: '{dirname}'")
-        try:
-            dir_instance = Directory(
-                dirname=dirname,
-                parent="B:/dolphin",
-                create_if_not_exists=True,
-                replacement_char="_",
-            )
-            frog.info(dir_instance)
-        except ValueError as e:
-            frog.error(f"Failed to create directory: {e}")
+    try:
+        dir_instance = Directory(
+            dirname="test",
+            parent="B:/dolphin",
+            replacement_char="_",
+        )
+        frog.info(dir_instance)
+        dir_instance.create()
+        dir_instance.rename("test1234")
+        frog.info(dir_instance)
+    except ValueError as e:
+        frog.error(f"Failed to create directory: {e}")
